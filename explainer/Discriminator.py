01f16569a7f26afcbdfc8a45ac279458bdbfdfe3
rizvansky <rizvaninno@gmail.com>
2020-09-28 20:28:25 +0400

    Changed the Discriminator module and some helper modules.

--- explainer/Discriminator.py
+++ explainer/Discriminator.py
@@ -7,25 +7,34 @@ class Discriminator(pl.LightningModule):
     def __init__(self):
         super().__init__()
 
+        self.d_res_block1 = DiscriminatorResBlock(in_channels=3, out_channels=64)
+        self.d_res_block2 = DiscriminatorResBlock(in_channels=64, out_channels=128)
+        self.d_res_block3 = DiscriminatorResBlock(in_channels=128, out_channels=256)
+        self.d_res_block4 = DiscriminatorResBlock(in_channels=256, out_channels=512)
+        self.d_res_block5 = DiscriminatorResBlock(in_channels=512, out_channels=1024, is_down=False)
+        self.relu = nn.ReLU()
+        self.global_sum_pooling = GlobalSumPooling()
+        self.inner_product = InnerProduct(in_channels=1024, n_classes=2)
+        self.dense = Dense(in_channels=1024, out_channels=1, is_sn=True)
+
     def forward(self, x, y, n_classes):
-        x = DiscriminatorResBlock(in_channels=3, out_channels=64)(x)
-        x = DiscriminatorResBlock(in_channels=64, out_channels=128)(x)
-        x = DiscriminatorResBlock(in_channels=128, out_channels=256)(x)
-        x = DiscriminatorResBlock(in_channels=256, out_channels=512)(x)
-        x = DiscriminatorResBlock(in_channels=512, out_channels=1024, is_down=False)(x)
+        x = self.d_res_block1(x)
+        x = self.d_res_block2(x)
+        x = self.d_res_block3(x)
+        x = self.d_res_block4(x)
+        x = self.d_res_block5(x)
 
-        x = nn.ReLU()(x)
+        x = self.relu(x)
 
-        x = GlobalSumPooling()(x)
+        x = self.global_sum_pooling(x)
 
         for i in range(0, n_classes - 1):
             if i == 0:
-                temp = InnerProduct(x, y[:, i + 1], 2)
+                temp = self.inner_product(x, y[:, i + 1], 2)
             else:
-                temp += InnerProduct(x, y[:, i + 1], 2)
+                temp += self.inner_product(x, y[:, i + 1], 2)
 
-        x = Dense()(x, 1, is_sn=True)
-
+        x = self.dense(x, 1, is_sn=True)
         x += temp
 
         return x
--- explainer/utils.py
+++ explainer/utils.py
@@ -1,34 +1,54 @@
 import pytorch_lightning as pl
 import torch.nn as nn
+import torch.nn.functional as F
 import torch
+
+
+class Downsampling(pl.LightningModule):
+    def __init__(self, kernel_size, stride):
+        super().__init__()
+
+        self.kernel_size = kernel_size
+        self.stride = stride
+
+    def forward(self, x):
+
+        #   input dimension (height or width)
+        dim_in = x.shape[1]
+
+        #   padding = 'SAME'
+        padding_h = int((dim_in * (self.stride[0] - 1) + self.kernel_size[0] - self.stride[0]) / 2)
+        padding_w = int((dim_in * (self.stride[1] - 1) + self.kernel_size[1] - self.stride[1]) / 2)
+        x = F.avg_pool2d(self.kernel_size, self.stride, padding=(padding_h, padding_w), input=x)
+
+        return x
 
 
 class Dense(pl.LightningModule):
     def __init__(self, in_channels, out_channels, is_sn=False):
         super().__init__()
 
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.is_sn = is_sn
+        self.fc = nn.Linear(in_channels, out_channels)
+
+        if is_sn:
+            self.fc = nn.utils.spectral_norm(self.fc)
 
     def forward(self, x):
-        w = nn.init.xavier_uniform(torch.empty(size=[self.in_channels, self.out_channels]))
+        x = self.fc(x)
 
-        if self.is_sn:
-            w = nn.utils.spectral_norm(w)
-
-        return torch.matmul(x, w)
+        return x
 
 
 class InnerProduct(pl.LightningModule):
-    def __init__(self, in_channels):
+    def __init__(self, in_channels, n_classes):
         super().__init__()
 
-        self.in_channels = in_channels
+        self.v = nn.init.xavier_uniform(torch.empty(size=[n_classes, in_channels]))
 
     def forward(self, x, y, n_classes):
-        v = nn.init.xavier_uniform(torch.empty(size=[n_classes, self.in_channels])).transpose(0, 1)
-        v = nn.utils.spectral_norm(v).transpose(0, 1)
+        self.v = self.v.transpose(0, 1)
+        self.v = nn.utils.spectral_norm(self.v)
+        self.v = self.v.transpose(0, 1)
 
         temp = torch.index_select(input=v, dim=0, index=y)
         temp = torch.sum(temp * x, 1, keepdim=True)
@@ -56,7 +76,8 @@ class DiscriminatorResBlock(pl.LightningModule):
         self.relu = nn.ReLU()
         self.conv1 = SpectralConv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, is_sn=is_sn)
         self.conv2 = SpectralConv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, is_sn=is_sn)
-        self.conv3 = SpectralConv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1, is_sn=is_sn)
+        self.conv_identity = SpectralConv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1, is_sn=is_sn)
+        self.downsampling = Downsampling(kernel_size=(1, 2), stride=(1, 2))
 
     def forward(self, x):
         temp = nn.Identity(x)
@@ -68,17 +89,15 @@ class DiscriminatorResBlock(pl.LightningModule):
         x = self.relu(x)
         x = self.conv2(x)
 
-        #   In the original implementation different kernel sizes and strides are applied for different dimensions
-        #   TODO check the kernel sizes, strides, and padding
         if self.is_down:
-            x = nn.AvgPool2d(kernel_size=2, padding=0)(x)
+            x = self.downsampling(x)
 
             if self.is_first:
-                temp = nn.AvgPool2d(kernel_size=2, padding=0)(temp)
+                temp = self.downsampling(temp)
                 temp = self.conv3(temp)
             else:
                 temp = self.conv3(temp)
-                temp = nn.AvgPool2d(kernel_size=2, padding=0)(temp)
+                temp = self.downsampling(temp)
 
         return x + temp
 
@@ -90,12 +109,11 @@ class SpectralConv2d(pl.LightningModule):
         #   Assuming that stride = 1
         padding = int(kernel_size - 1) / 2
         self.conv = nn.Conv2d(in_channels, out_channels, padding, kernel_size, stride)
-        self.is_sn = is_sn
+
+        if is_sn:
+            self.conv = nn.utils.spectral_norm(self.conv)
 
     def forward(self, x):
         x = self.conv(x)
-
-        if self.is_sn:
-            x = nn.utils.spectral_norm(x)
 
         return x
