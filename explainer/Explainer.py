1ce165c97d5e7e28f39edf298d5a4563dc51036c
rizvansky <rizvaninno@gmail.com>
2020-09-30 21:22:04 +0400

    Small fix in loss functions

--- explainer/Explainer.py
+++ explainer/Explainer.py
@@ -1,7 +1,8 @@
 import pytorch_lightning as pl
 from explainer.Discriminator import Discriminator
 from explainer.GeneratorEncoderDecoder import GeneratorEncoderDecoder
-from explainer.utils import DiscriminatorLoss, GeneratorLoss
+import torch
+import torch.nn.functional as F
 
 
 class Explainer(pl.LightningModule):
@@ -25,13 +26,39 @@ class Explainer(pl.LightningModule):
         self.G = GeneratorEncoderDecoder()
         self.D = Discriminator()
 
-        g_loss_gan = GeneratorLoss()
-        d_loss_gan = DiscriminatorLoss()
-
     def forward(self):
         pass
 
+    #   Code is borrowed from here:
+    #   https://github.com/PyTorchLightning/PyTorch-Lightning-Bolts/blob/master/pl_bolts/models/gans/basic/basic_gan_module.py#L10-L169
+    #   x_real, x_fake are truth image and generated image respectively
+    def discriminator_loss(self, x_real, x_fake, loss_func=F.multilabel_margin_loss):
+        b = x_real.size(0)
+        x_real = x_real.view(b, -1)
+        y_real = torch.ones(b, 1)
 
+        #   calculate real score
+        output = self.D(x_real)
+        real_loss = F.binary_cross_entropy(output, y_real)
 
+        y_fake = torch.zeros(b, 1)
+
+        # calculate fake score
+        output = self.D(x_fake)
+        fake_loss = loss_func(output, y_fake)
+
+        # gradient backpropagation & optimize ONLY D's parameters
+        loss = real_loss + fake_loss
+
+        return loss
+
+    def generator_loss(self, x_fake, loss_func=F.multilabel_margin_loss):
+        y_fake = torch.ones(x_fake.size(0), 1)
+
+        # calculate fake score
+        output = self.D(x_fake)
+        loss = loss_func(output, y_fake)
+
+        return loss
 
 
