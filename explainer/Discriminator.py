0d2c69b9af78f5a3f3032123e67be0e1ee322a49
rizvansky <rizvaninno@gmail.com>
2020-10-22 19:32:09 +0000

    Almost finished the evaluation of Disciminator module and added fixes in modules

--- explainer/Discriminator.py
+++ explainer/Discriminator.py
@@ -4,23 +4,26 @@ import torch.nn as nn
 
 
 class Discriminator(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, n_bins):
         super().__init__()
-
-        # Added that this block is first
+    
+        self.n_bins = n_bins
+        
         self.d_res_block1 = DiscriminatorResBlock(in_channels=3, out_channels=64, is_first=True)
         self.d_res_block2 = DiscriminatorResBlock(in_channels=64, out_channels=128)
         self.d_res_block3 = DiscriminatorResBlock(in_channels=128, out_channels=256)
         self.d_res_block4 = DiscriminatorResBlock(in_channels=256, out_channels=512)
         self.d_res_block5 = DiscriminatorResBlock(in_channels=512, out_channels=1024)
-        # 6 layers, not 5
-        self.d_res_block6 = DiscriminatorResBlock(in_channels=1024, out_channels=1024)
+        self.d_res_block6 = DiscriminatorResBlock(in_channels=1024, out_channels=1024, is_down=False)
+        
         self.relu = nn.ReLU()
-        self.global_sum_pooling = GlobalSumPooling()
+        self.global_sum_pooling = nn.AvgPool2d(kernel_size=4)
         self.inner_product = InnerProduct(in_channels=1024, n_classes=2)
-        self.dense = Dense(in_channels=1024, out_channels=1, is_sn=True)
-
-    def forward(self, x, y, n_classes):
+        self.fc = nn.utils.spectral_norm(nn.Linear(1024, 1))
+    
+     #  Here I deleted y argument in def forward
+     #  TODO need to repair! add y argument (and fix inner-product function) and evaluate the total size
+    def forward(self, x):
         x = self.d_res_block1(x)
         x = self.d_res_block2(x)
         x = self.d_res_block3(x)
@@ -32,14 +35,15 @@ class Discriminator(pl.LightningModule):
 
         x = self.global_sum_pooling(x)
 
-        for i in range(0, n_classes - 1):
-            if i == 0:
-                temp = self.inner_product(x, y[:, i + 1])
-            else:
-                temp += self.inner_product(x, y[:, i + 1])
+        # for i in range(0, self.n_bins - 1):
+            # if i == 0:
+                # temp = self.inner_product(x, y[:, i + 1])
+            # else:
+                # temp += self.inner_product(x, y[:, i + 1])
 
-        x = self.dense(x)
+        x = x.view(-1, 1024)
+        x = self.fc(x)
 
-        x += temp
+        # x += temp
 
         return x
