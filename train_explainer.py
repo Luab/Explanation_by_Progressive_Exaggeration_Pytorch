4c3989f299a6ddfb59d992713c374d4fed5d3412
rizvansky <rizvaninno@gmail.com>
2020-11-05 11:36:30 +0000

    C1

--- train_explainer.py
+++ train_explainer.py
@@ -8,15 +8,6 @@ import yaml
 import os
 import subprocess as sp
 
-def get_gpu_memory():
-        _output_to_list = lambda x: x.decode('ascii').split('\n')[:-1]
-
-        ACCEPTABLE_AVAILABLE_MEMORY = 1024
-        COMMAND = "nvidia-smi --query-gpu=memory.free --format=csv"
-        memory_free_info = _output_to_list(sp.check_output(COMMAND.split()))[1:]
-        memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
-        return memory_free_values[0]
-
 #   TODO synchronize path with saved models with path for saving in config file
 def main():
     # print('***************************')
