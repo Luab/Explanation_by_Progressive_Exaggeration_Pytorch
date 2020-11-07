Turned off gradients of model to save memory, fixed the bug with the target class

classifier/DenseNet.py
@@ -7,7 +7,7 @@ import torch
 class DenseNet121(pl.LightningModule):
     def __init__(self, config):
         super().__init__()
-        
+
         self.batch_size = config['batch_size']
         self.epochs = config['epochs']
         self.channels = config['num_channel']
@@ -17,28 +17,25 @@ class DenseNet121(pl.LightningModule):
         self.model = models.densenet121(pretrained=True)
         num_ftrs = self.model.classifier.in_features
         self.model.classifier = nn.Linear(num_ftrs, self.n_classes)
-        
-        self.loss = nn.BCEWithLogitsLoss()
 
+        self.loss = nn.BCEWithLogitsLoss()
 
     def forward(self, x):
         return self.model(x)
 
-    
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.model.parameters(), lr=0.0001)
         return optimizer
 
-    
     def training_step(self, batch, batch_idx):
         images, targets = batch
         outputs = self(images)
+        print("outputs shape:", outputs.shape)
         train_loss = self.loss(outputs, targets)
         result = pl.TrainResult(minimize=train_loss)
         result.log('train_loss', train_loss, on_epoch=True)
         return result
 
-    
     def validation_step(self, batch, batch_idx):
         images, targets = batch
         outputs = self(images)

explainer/Explainer.py
@@ -35,8 +35,11 @@ class Explainer(pl.LightningModule):
         cls_ckpt_path = os.path.join(config['cls_experiment'], 'last.ckpt')
         cls_ckpt = torch.load(cls_ckpt_path)
         self.model.load_state_dict(cls_ckpt['state_dict'])
-        self.model.eval()
-    
+
+        # Free memory that is reserved for gradients
+        for p in self.model.parameters():
+            p.requires_grad_(False)
+
     def get_gpu_memory(self):
         _output_to_list = lambda x: x.decode('ascii').split('\n')[:-1]
 
@@ -45,7 +48,7 @@ class Explainer(pl.LightningModule):
         memory_free_info = _output_to_list(sp.check_output(COMMAND.split()))[1:]
         memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
         return memory_free_values[0]
-    
+
     def forward(self, x, y):
         return self.G(x, y)
 
@@ -59,15 +62,15 @@ class Explainer(pl.LightningModule):
         x_source, y_s = batch
         y_s = y_s.view(-1)
         y_s = self.convert_ordinal_to_binary(y_s, self.n_bins)
-        
+
         y_t = torch.randint(low=0, high=self.n_bins, size=[self.batch_size])
-        y_t = self.convert_ordinal_to_binary(y_t, self.n_bins) 
+        y_t = self.convert_ordinal_to_binary(y_t, self.n_bins)
 
         result = None
 
         if batch_idx % 5 == 0 and optimizer_idx == 0:
             result = self.generator_step(x_source, y_t, y_s)
-            
+
         if optimizer_idx == 1:
             result = self.discriminator_step(x_source, y_t, y_s)
 
@@ -82,23 +85,23 @@ class Explainer(pl.LightningModule):
         y_target = y_t[:, 0]
         y_source = y_s[:, 0]
         # print('Free GPU memory before generator training step: {} MB'.format(self.get_gpu_memory()))
-        
+
         # print('fake_target_img, fake_target_img_embedding calculating...')
-        fake_target_img, fake_target_img_embedding = self(x_source, y_target)
+        fake_target_img, fake_target_img_embedding = self.G(x_source, y_target)
         # print('Free GPU memory after fake_target_img, fake_target_img_embedding calculating: {} MB\n\n'.format(self.get_gpu_memory()))
-        
+
         # print('fake_source_recons_img, x_source_img_embedding calculating...')
-        fake_source_recons_img, x_source_img_embedding = self(x_source, y_source)
+        fake_source_recons_img, x_source_img_embedding = self.G(x_source, y_source)
         # print('Free GPU memory after fake_source_recons_img, x_source_img_embedding calculating: {} MB\n\n'.format(self.get_gpu_memory()))
-        
+
         # print('fake_source_img, fake_source_img_embedding calculating...')
-        fake_source_img, fake_source_img_embedding = self(fake_target_img, y_source)
+        fake_source_img, fake_source_img_embedding = self.G(fake_target_img, y_source)
         # print('Free GPU memory after fake_source_img, fake_source_img_embedding calculating: {} MB\n\n'.format(self.get_gpu_memory()))
-        
+
         # print('g_loss_rec calculating...')
         g_loss_rec = F.mse_loss(x_source_img_embedding, fake_source_img_embedding)
         # print('Free GPU memory after g_loss_rec calculating: {} MB\n\n'.format(self.get_gpu_memory()))
-        
+
         fake_target_logits = self.D(fake_target_img, y_t)
         # print('Free GPU memory after Discriminator forward propagation: {} MB\n\n'.format(self.get_gpu_memory()))
 
@@ -108,29 +111,23 @@ class Explainer(pl.LightningModule):
 
         fake_img_cls_logit_pretrained = self.model(fake_target_img)
         fake_img_cls_prediction = torch.sigmoid(fake_img_cls_logit_pretrained)
-        fake_q = fake_img_cls_prediction[:, self.target_class]
+        # Here we have only one class, that why either we choose self.target_class - 1, or slices
+        fake_q = fake_img_cls_prediction[:, :self.target_class]
         real_p = y_target.clone().detach() * 0.1
         fake_evaluation = real_p * torch.log(fake_q) + (1 - real_p) * torch.log(1 - fake_q)
 
-        # real_img_recons_cls_logit_pretrained = self.model(fake_source_img)
-        # real_img_recons_cls_prediction = torch.sigmoid(real_img_recons_cls_logit_pretrained)
-        # real_img_cls_logits_pretrained = self.model(x_source)
-        # real_img_cls_prediction = torch.sigmoid(real_img_cls_logits_pretrained)
-        # recons_evaluation = real_img_cls_prediction[:, self.target_class] * torch.log(
-        #     real_img_recons_cls_prediction[:, self.target_class]) + (
-        #                                 1 - real_img_recons_cls_prediction[:, self.target_class]) * torch.log(
-        #     1 - real_img_recons_cls_prediction[:, self.target_class]) #  F.binary_cross_entropy
-        # recons_evaluation -= torch.mean(recons_evaluation) # May be =, not -=?
 
         # Переписал с использованием binary_cross_entropy_with_logits, мб меньше памяти будет жрать
         real_img_recons_cls_logit_pretrained = self.model(fake_source_img)
         real_img_cls_logits_pretrained = self.model(x_source)
+        # Here we have only one class, that why either we choose self.target_class - 1, or slices
         recons_evaluation = F.binary_cross_entropy_with_logits(
-            real_img_recons_cls_logit_pretrained[:, self.target_class],
-            real_img_cls_logits_pretrained[:, self.target_class]
+            real_img_recons_cls_logit_pretrained[:, :self.target_class],
+            real_img_cls_logits_pretrained[:, :self.target_class]
         )
 
         g_loss = g_loss_gan * self.lambda_gan + g_loss_rec * self.lambda_cyc + g_loss_cyc * self.lambda_cyc + fake_evaluation * self.lambda_cls + recons_evaluation * self.lambda_cls
+        print("g_loss type:", type(g_loss), g_loss.shape)
 
         result = pl.TrainResult(minimize=g_loss, checkpoint_on=g_loss)
         # Passed value to logging, may be we need to pass all values (recons_evaluation, fake_evaluation losses) to track it?
@@ -140,7 +137,7 @@ class Explainer(pl.LightningModule):
 
     def discriminator_step(self, x_source, y_t, y_s):
         # print("d")
-        
+
         x_source, y_t, y_s = x_source.cuda(), y_t.cuda(), y_s.cuda()
         y_target = y_t[:, 0]
 
@@ -194,7 +191,6 @@ class Explainer(pl.LightningModule):
         loss = fake_loss
 
         return loss
-
 
     # Instead of this we could use torch.nn.functional.one_hot, numpy is super slow
     @staticmethod