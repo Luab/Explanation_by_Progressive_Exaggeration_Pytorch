0627005277413c89b4e49b8d593d312ac8269052
rizvansky <rizvaninno@gmail.com>
2020-11-12 23:23:26 +0000

    Added validation and tensorboard logging

--- train_explainer.py
+++ train_explainer.py
@@ -8,43 +8,35 @@ import yaml
 import os
 import subprocess as sp
 
-#   TODO synchronize path with saved models with path for saving in config file
+
 def main():
-    # print('***************************')
-    # print(get_gpu_memory())
-    # print('***************************')
-    
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', '-c', default='/home/intern/BS19_implementation/configs/celebA_Young_Explainer.yaml')
+    parser.add_argument('--config', '-c', default='configs/celebA_Young_Explainer.yaml')
     args = parser.parse_args()
 
     config_path = args.config
     with open(config_path) as f:
         config = yaml.safe_load(f)
 
-    # They simply save each 500 iterations, but I monitor only min g_loss
     checkpoint_callback = ModelCheckpoint(
-        filepath=os.path.join('checkpoints/explainer', config['name'], 'model.ckpt'),
+        filepath=os.path.join('checkpoints/explainer', config['name'], 'explainer'),
         save_last=True,
         save_top_k=1,
-        monitor='g_loss',
+        monitor='val_loss_abs',
         verbose=True,
-        mode='min',
+        mode='min'
     )
-    # Let's for now skip logger it until training will start
-    # logger = TensorBoardLogger(config['log_dir'], name=config['name'])
+    
+    logger = TensorBoardLogger(config['log_dir'], name=config['name'])
     data_module = DataModule(config, from_explainer=True)
     train_loader = data_module.train_dataloader()
     val_loader = data_module.val_dataloader()
 
-    # In Explainer we shouldn't pass logger, only into Trainer
     explainer = Explainer(config)
 
-    # Let's for now skip logger it until training will start
-    # By setting val_percent_check=0.0 validation would not execute
-    trainer = pl.Trainer(gpus=1, max_epochs=explainer.epochs, limit_val_batches=50, limit_train_batches=50)
+    trainer = pl.Trainer(gpus=1, max_epochs=explainer.epochs, limit_val_batches=50, limit_train_batches=50, logger=logger, callbacks=[checkpoint_callback])
 
-    trainer.fit(explainer, train_loader)
+    trainer.fit(explainer, train_loader, val_loader)
 
 
 if __name__ == '__main__':
